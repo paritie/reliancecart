0db3f8b20b8da747955991ee5f45543040ef78c4
mukhtar adaji <mukhtar@paritie.com>
2021-10-08 16:41:43 -0700

    added bootstrap carousel the make a background image slide at the home page

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ slider.oninput = function() {
   kg.innerHTML = this.value + 'Kg';
   mins.innerHTML = Math.floor(slider.value * 10.4) + 'Mins'
 }
+
+// const hero_slider = document.querySelector('.hero-slider');
+// let left = 0
+//  setInterval(() => {
+//    left = left - 100
+//    if(left == -300){
+//      left = 400
+//    }
+//   hero_slider.style.left = left + '%'
+//  }, 4000)
+
+
