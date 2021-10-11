e3f6e374ea4c6e106ac88a167eec93bebd9cfd11
mukhtar adaji <mukhtar@paritie.com>
2021-10-10 21:57:41 -0700

    made some adjustment to the mobile face of the home page

--- js/main.js
+++ js/main.js
@@ -9,14 +9,5 @@ slider.oninput = function() {
   mins.innerHTML = Math.floor(slider.value * 10.4) + 'Mins'
 }
 
-// const hero_slider = document.querySelector('.hero-slider');
-// let left = 0
-//  setInterval(() => {
-//    left = left - 100
-//    if(left == -300){
-//      left = 400
-//    }
-//   hero_slider.style.left = left + '%'
-//  }, 4000)
 
 
